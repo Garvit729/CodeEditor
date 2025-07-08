b134d39a1bc8d8f39df91a4eb82495e7883e6475
Garv729 <garvitjain729@gmail.com>
2025-07-08 23:41:20 +0530

    cors updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://code-editor-two-green.vercel.app/'],
     credentials: true 
 }))
 
