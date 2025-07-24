8496d0c72f904272233f5c7f5fd2b23a48e8e4dc
Garv729 <garvitjain729@gmail.com>
2025-07-25 01:23:12 +0530

    ok1

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://codeback-1.onrender.com',
+    baseURL: 'https://codeback-1.onrender.com/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
